d13512d89d3b7a07da11e4188bb29324c64325a6
Quinn Le <quynhnle135@gmail.com>
2024-01-03 09:06:16 -0500

    Deploy website - based on adfcc0989b2956a84ffeac71deb7020d51ee2e03

---
+++ assets/js/3c6321a2.a476de05.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[5622],{1029:e=>{e.exports=JSON.parse('{"title":"Journals","slug":"/category/journals","permalink":"/docs/category/journals","navigation":{"next":{"title":"Jornals - 2023","permalink":"/docs/category/jornals---2023"}}}')}}]);
